d26fa1b4fc18ce46f0b2f2d9ca7a91e2b3a423a6
Akshit <46820300+codestromer@users.noreply.github.com>
2020-11-04 01:47:45 +0530

    Create array-7-Next Permutation(Leetcode-31).cpp

---
+++ Array/array-7-Next Permutation(Leetcode-31).cpp
@@ -0,0 +1,20 @@
+/* 2D-array -- https://leetcode.com/problems/next-permutation/ */
+
+
+/* Solution-1 => Using Inbuilt function */
+/* Time comp. - O() */
+/* Space comp. - O() */
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // #include<algorithm>
+        next_permutation(nums.begin(),nums.end());
+    }
+};
+
+
+/* Solution-2 */
+/* Time comp. - O() */
+/* Space comp. - O() */
+
