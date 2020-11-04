874e1073480ae1b056ae137b2af0aaff35e9d6f6
Akshit <46820300+codestromer@users.noreply.github.com>
2020-11-04 13:36:24 +0530

    Update array-7-Next Permutation(Leetcode-31).cpp

--- Array/array-7-Next Permutation(Leetcode-31).cpp
+++ Array/array-7-Next Permutation(Leetcode-31).cpp
@@ -14,7 +14,61 @@ public:
 };
 
 
-/* Solution-2 */
-/* Time comp. - O() */
-/* Space comp. - O() */
+/* Solution-2 (Intuition - dictionary order)*/
+/* Time comp. - O(N^2) */
+/* Space comp. - O(1) */
 
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 1) return;
+        bool done = 0;
+        
+        for(int j = n - 2; j >= 0;j--){
+            for(int k = n - 1; k > j;k--){
+                if(nums[j] < nums[k]){
+                    swap(nums[j],nums[k]);
+                    reverse(nums.begin()+j+1,nums.end());
+                    done = 1;
+                    break;
+                }
+            }
+            if(done) break;
+        }
+        if(!done) 
+        {
+            int i = 0, j = n-1;
+            while(i<=j)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+        }
+    }
+};
+
+/* Solution-3 (Same as 2)*/
+/* Time comp. - O(N) */
+/* Space comp. - O(1) */
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& a) {
+        int n = a.size(), i, j;
+        if(n<=1)    return;
+        
+        for(i = n-2; i>=0;i--)
+            if(a[i] < a[i+1])
+                break;
+        
+        if(i>=0){
+            for(j = n-1; j > i; j--)
+                if(a[j] > a[i])
+                    break;
+            swap(a[i],a[j]);
+        }
+        reverse(a.begin()+i+1,a.end()); 
+    }
+};
