003261511e6440040b9b5b52a504c80fed5027b2
Akshit <46820300+codestromer@users.noreply.github.com>
2020-11-17 13:39:17 +0530

    Update linked-list-1-Reverse Linked List(Leetcode-206).cpp

--- Linked-List/linked-list-1-Reverse Linked List(Leetcode-206).cpp
+++ Linked-List/linked-list-1-Reverse Linked List(Leetcode-206).cpp
@@ -1,5 +1,10 @@
 /* Problem-Link:- https://leetcode.com/problems/reverse-linked-list */
 
+
+/* Solution-1 => Iterative */
+/* Time comp. - O(N) */
+/* Space comp. - O(1) */
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -11,5 +16,24 @@ public:
             current = nxt;
         }
         return previous;
+    }
+};
+
+
+/* Solution-2 => Recursive */
+/* Time comp. - O(N) */
+/* Space comp. - O(N) */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode* rest = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+ 
+        return rest;
     }
 };
